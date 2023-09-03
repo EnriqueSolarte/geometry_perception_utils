2dc60263fafd03302ff5c80ed80e5733ff1d1b97
EnriqueSolarte <enrique.solarte.pardo@gmail.com>
2023-09-03 12:32:25 +0800

    Fix issue to get REPO version

--- geometry_perception_utils/__init__.py
+++ geometry_perception_utils/__init__.py
@@ -2,4 +2,4 @@ import os
 from geometry_perception_utils.config_utils import get_repo_version
 DIR_GEOM_UTILS = os.path.dirname(os.path.abspath(__file__))
 
-VERSION = get_repo_version(DIR_GEOM_UTILS)
+VERSION = get_repo_version(__file__)
