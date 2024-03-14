9e46b742f44c2b8554840e7b1689470df521d16a
EnriqueSolarte <enrique.solarte.pardo@gmail.com>
2024-03-14 09:19:29 +0800

    Add warping utils

---
+++ geometry_perception_utils/warping/equi2equi.py
@@ -0,0 +1,38 @@
+
+# This lib is a wrapper of equilib
+from equilib import Equi2Equi
+import numpy as np
+import torch
+from geometry_perception_utils.warping.utils import preprocess, postprocess
+from imageio.v2 import imwrite, imread
+
+
+def main():
+    img_equi_fn='/media/Pluto/kike/geometry_perception_utils/geometry_perception_utils/data/equi2.png'
+    results_dir = '/media/Pluto/kike/geometry_perception_utils/geometry_perception_utils/data/results'
+    # Rotation:
+    rot = {
+        "roll": 0,  #
+        "pitch": np.pi / 4,  # vertical
+        "yaw": 0,  # horizontal
+    }
+
+    # Initialize equi2equi
+    equi2equi = Equi2Equi()
+    device = torch.device("cuda")
+
+    # Open Image
+    src_img = imread(img_equi_fn)[:, :, :3]
+    src_img = preprocess(src_img).to(device)
+
+    out_img = equi2equi(src=src_img, rots=rot)
+    out_img = postprocess(out_img)
+    out_img = np.array(out_img)
+    out_path = f"{results_dir}/warping_equi2equi.jpg"
+    imwrite(out_path, out_img)
+    
+    print(f"Save in {out_path}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ geometry_perception_utils/warping/utils.py
@@ -0,0 +1,40 @@
+
+import numpy as np
+from PIL import Image
+import torch
+from torchvision import transforms
+from typing import Union
+import cv2
+
+def preprocess(
+    img: Union[np.ndarray, Image.Image], is_cv2: bool = False
+) -> torch.Tensor:
+    """Preprocesses image"""
+    if isinstance(img, np.ndarray) and is_cv2:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    if isinstance(img, Image.Image):
+        # Sometimes images are RGBA
+        img = img.convert("RGB")
+
+    to_tensor = transforms.Compose([transforms.ToTensor()])
+    img = to_tensor(img)
+    assert len(img.shape) == 3, "input must be dim=3"
+    assert img.shape[0] == 3, "input must be HWC"
+    return img
+
+
+def postprocess(
+    img: torch.Tensor, to_cv2: bool = False
+) -> Union[np.ndarray, Image.Image]:
+    if to_cv2:
+        img = np.asarray(img.to("cpu").numpy() * 255, dtype=np.uint8)
+        img = np.transpose(img, (1, 2, 0))
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+        return img
+    else:
+        to_PIL = transforms.Compose([transforms.ToPILImage()])
+        img = img.to("cpu")
+        img = to_PIL(img)
+        return img
+
+
