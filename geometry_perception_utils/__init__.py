570039d48711ce37577cde4776f99ce927fa90eb
EnriqueSolarte <enrique.solarte.pardo@gmail.com>
2024-09-13 14:37:47 +0800

    Fix config_utils in the main __init__ of the lib

--- geometry_perception_utils/__init__.py
+++ geometry_perception_utils/__init__.py
@@ -1,6 +1,6 @@
+import geometry_perception_utils.config_utils
 import os
 DIR_GEOM_UTILS = os.path.dirname(os.path.abspath(__file__))
 
-import config_utils 
 # from geometry_perception_utils.config_utils import get_repo_version
 # VERSION = get_repo_version(__file__)
